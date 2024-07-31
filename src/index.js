fee944067314f754357735c2943b42d5081fc29e
tom mckenna <thomasmckenna12@gmail.com>
2024-07-30 22:17:12 -0500

    added papaparse dependencies + starter code + initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const papa = require('papaparse')
+
+
+
+
+
+
+// const fs = require("fs")
+// const https = require("http")
+
+// fs.writeFile("./src/test.txt", "utf-8", () => {
+//     console.log("file created")
+// })
