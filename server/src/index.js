391b37df300faa8969af227d36e43dfaf426c726
Thomas <thomasmckenna12gmail.com>
2024-08-17 16:24:15 -0500

    added matthew perrys book notes

--- server/src/index.js
+++ server/src/index.js
@@ -1,68 +1,70 @@
-
-
-const mdw = "/Users/thomasmckenna/Downloads/Million-Dollar-Weekend-Notes.csv"
+const millionDollarWeekend = "/Users/thomasmckenna/Downloads/Million-Dollar-Weekend-Notes.csv"
+const howToBeFree = "/Users/thomasmckenna/Downloads/how-to-be-free.csv"
+const friendsLoversAndTheBigTerribleThing = "/Users/thomasmckenna/Downloads/friends-lovers-and-the-big-terrible-thing-a-memoir.csv"
+console.log(friendsLoversAndTheBigTerribleThing)
 const csv = require('csv-parser')
 const fs = require('fs')
-const results = [];
 const nodemailer = require('nodemailer')
-
 const noteFile = "/Users/thomasmckenna/kindle-notes-app/HighlightedNotes/highlights.txt"
 
-// pull and read the data
-fs.createReadStream(mdw)
+// pull notes and read them
+// how to notes from each book 
+// put them in one list/object/json file
+// pull 10 random notes from that file
+
+
+
+
+const results = [];
+fs.createReadStream(friendsLoversAndTheBigTerribleThing)
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
-
-    const textData = JSON.stringify(results[12])
-
-// writing a file for your highlighted notes
+    const textData = JSON.stringify(results[19])
+    // console.log(textData)
     fs.writeFile(noteFile, textData, (err) => {
         if (err) {
             console.log("error writing file", err)
         } else {
             console.log('file has been written')
-            
+            console.log(results[19])
         }
     })
 });
 
+
 // starter code for email
 // sending emails
 // Create a transporter object
-const transporter = nodemailer.createTransport({
-    // host: 'live.smtp.mailtrap.io', 
-    // host: 'sandbox.smtp.mailtrap.io', 
-    host: 'smtp.gmail.com', 
-    port: 587,
-    secure: false, // use SSL
-    auth: {
-        // user: '1d469bd30f4a48',
-        // pass: 'f1e6ed2c7b83ed',
-        user: 'thomasmckenna12@gmail.com ',
-        pass: 'abcj zpwy bcwo ibvu',
-    }
-});
+// const transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com', 
+//     port: 587,
+//     secure: false, // use SSL
+//     auth: {
+//         user: 'thomasmckenna12@gmail.com ',
+//         pass: 'abcj zpwy bcwo ibvu',
+//     }
+// });  
 
-// Configure the mailoptions object
-const mailOptions = {
-    from: 'thomasmckenna12@gmail.com',
-    to: 'thomasmckenna12@gmail.com',
-    subject: 'Sending Email using Node.js',
-    text: 'This is my first email WOOOOOOOO', 
-    attachments: [
-        {
-            filename: 'hightlights.txt', 
-            path: noteFile,
-        }
-    ]
-};
+// // Configure the mailoptions object
+// const mailOptions = {
+//     from: 'thomasmckenna12@gmail.com',
+//     to: 'thomasmckenna12@gmail.com',
+//     subject: 'Sending Email using Node.js', // this will be a variable
+//     text: 'This is my first email WOOOOOOOO',  // this will be a variable
+//     attachments: [
+//         {
+//             filename: 'hightlights.txt', 
+//             path: noteFile,
+//         }
+//     ]
+// };
 
-  // Send the email
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log('Error:', error);
-    } else {
-        console.log('Email sent:', info.response);
-    }
-});
+//   // Send the email
+// transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//         console.log('Error:', error);
+//     } else {
+//         console.log('Email sent:', info.response);
+//     }
+// });
